5e748d81128fae4ba20de12899a54b7f3fe97a7d
Wil Poole <wilpoole@Wils-MacBook-Pro.local>
2017-11-26 22:14:35 +0000

    Last update

--- Part 1 - Data Preprocessing/wil_data_preprocessing.py
+++ Part 1 - Data Preprocessing/wil_data_preprocessing.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-# Data Preprocessing
+# File information
 """
 wil_data_preprocessing.py: First look at preprocessing data.
 """
-
 __author__ = "Wil Poole"
 __copyright__ = "2017"
 __credits__ = "Wil Poole"
@@ -14,18 +13,26 @@ __maintainer__ = "Wil Poole"
 __email__ = "@"
 __status__ = "Development"
 
+
+
 # Import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 from os import path
+from sklearn.preprocessing import Imputer
 
 # Variables
 data_directory = "data"
 data_file = "data.csv"
 
+
+
+
 # Data import
 data_set = pd.read_csv(path.join(data_directory, data_file))
+
 
 # Data format
 data_set_dependent = data_set.iloc[ : , :-1 ].values
--- Part 1 - Data Preprocessing/wil_data_preprocessing.r
+++ Part 1 - Data Preprocessing/wil_data_preprocessing.r
@@ -1,1 +1,9 @@
 # Data preprocessing
+#
+
+# Variables
+data.directory = "data"
+data.file = "data.csv"
+
+# Data import
+data.set = read.csv(file.path(data.directory, data.file))
