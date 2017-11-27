cd1b414f4a0da7b010d18c80c9fd42d59b9c4d4a
Wil Poole <wilpoole@Wils-MacBook-Pro.local>
2017-11-27 14:03:03 +0000

    Final version

--- Part 1 - Data Preprocessing/wil_data_preprocessing.py
+++ Part 1 - Data Preprocessing/wil_data_preprocessing.py
@@ -14,26 +14,46 @@ __email__ = "@"
 __status__ = "Development"
 
 
-
 # Import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 from os import path
-from sklearn.preprocessing import Imputer
+
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 # Variables
 data_directory = "data"
 data_file = "data.csv"
 
-
-
+# Models
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
 
 # Data import
 data_set = pd.read_csv(path.join(data_directory, data_file))
 
-
 # Data format
 data_set_dependent = data_set.iloc[ : , :-1 ].values
 data_set_independent = data_set.iloc[ : , -1 ].values
+
+data_set_dependent = imputer.fit_transform(data_set_dependent[ : , 1:3 ])
+
+data_set_dependent[:, 0] = LabelEncoder().fit_transform(
+  data_set_dependent[:, 0]
+  )
+
+data_set_dependent = OneHotEncoder(
+  categorical_features = [0]
+  ).fit_transform(data_set_dependent).toarray()
+
+data_set_independent = LabelEncoder().fit_transform(data_set_independent)
+
+data_set_dependent_train, data_set_dependent_test, data_set_independent_train, data_set_independent_test = train_test_split(
+  data_set_dependent, data_set_independent, test_size = 0.2, random_state = 0
+  )
+
+dependent_scaler = StandardScaler()
+data_set_dependent_train = dependent_scaler.fit_transform(data_set_dependent_train)
+data_set_dependent_test = dependent_scaler.transform(data_set_dependent_test)
