44943cd5f46376e25098e900e67bc525a206a242
Wil Poole <wilpoole@gmail.com>
2018-04-08 13:29:18 +0100

    Finished regression

--- Part 2 - Regression/Section 9 - Random_Forest_Regression/wil_random_forest_regression.py
+++ Part 2 - Regression/Section 9 - Random_Forest_Regression/wil_random_forest_regression.py
@@ -9,3 +9,21 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+
+# Fitting the Model
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X,y)
+
+# Predict the value
+regressor.predict(6.5)
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
